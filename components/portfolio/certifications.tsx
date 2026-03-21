85cb98c659ee2834224dcf39d4246e46994f78f4
SauravSrivastava27 <saurav.srivastava27@outlook.com>
2026-03-21 17:08:59 +0530

    chnage description

--- components/portfolio/certifications.tsx
+++ components/portfolio/certifications.tsx
@@ -52,7 +52,7 @@ export function Certifications() {
           <div className="h-px bg-border flex-1 max-w-xs" />
         </div>
 
-        <div className="grid md:grid-cols-2 gap-8">
+        <div className="grid gap-8">
           {/* Certifications */}
           <div>
             <div className="flex items-center gap-2 mb-6">
